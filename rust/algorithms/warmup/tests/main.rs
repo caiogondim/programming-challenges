9a08c882063db13e7c69ca13ceafa4f36f704756
Caio Gondim <me@caiogondim.com>
2018-06-01 13:58:02 -0400

    Tests for rust/algorithms/warm-up

--- rust/algorithms/warmup/tests/main.rs
+++ rust/algorithms/warmup/tests/main.rs
@@ -1,10 +1,22 @@
+use std::fs::File;
+use std::io::prelude::*;
+
 extern crate assert_cli;
 
 #[test]
 fn main() {
+  let mut input_file = File::open("./tests/input.txt").expect("file not found");
+  let mut input = String::new();
+  input_file.read_to_string(&mut input).expect("error while reading file");
+
+  let mut output_file = File::open("./tests/output.txt").expect("file not found");
+  let mut output = String::new();
+  output_file.read_to_string(&mut output).expect("error while reading file");
+  let output_ref: &str = output.as_ref();
+
   assert_cli::Assert::command(&["./target/debug/warmup"])
-    .stdin("2\n3")
+    .stdin(input)
     .stdout()
-    .is("5")
+    .is(output_ref)
     .unwrap();
 }
