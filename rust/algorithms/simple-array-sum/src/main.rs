f25710b4650ec19c0029bd66ee30fb2702129e4b
Caio Gondim <me@caiogondim.com>
2018-06-02 23:10:24 -0400

    Simplify rust/algorithms/warmup/simple-array-sum

--- rust/algorithms/simple-array-sum/src/main.rs
+++ rust/algorithms/simple-array-sum/src/main.rs
@@ -11,8 +11,7 @@ fn main() {
     .read_line(&mut input2)
     .expect("failed to read line from stdin");
   let iter = input2.split_whitespace().map(|x| x.parse::<i64>().expect("unable to parse"));
-  let num_vec: Vec<i64> = iter.collect();
-  let sum: i64 = num_vec.iter().sum();
+  let sum: i64 = iter.sum();
 
   println!(" {} ", sum);
 }
